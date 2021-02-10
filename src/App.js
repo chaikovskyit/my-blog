e77d9198e1c2afd27b92868b03501943c0b791f5
chaikovskyit <chaikovskyit@gmail.com>
2021-02-10 18:52:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserInfo from './components/UserInfo';
+import s from './App.module.css';
 
-function App() {
+const App = () => {
+  const [killerNumber, setKillerNumber] = useState(1);
+  const [showAll, setShowAll] = useState(false);
+
+  const inc = () => setKillerNumber(killerNumber + 1);
+  const dec = () => setKillerNumber(killerNumber - 1);
+  const show = () => setShowAll(true);
+  const unshow = () => setShowAll(false);
+
+  if (killerNumber > 10) {
+    return setKillerNumber(1);
+  }
+  if (killerNumber < 1) {
+    return setKillerNumber(10);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.wrapper}>
+      <h1>We have 10 killers</h1>
+      <h2>
+        Change killer number and look info about him :
+        {' '}
+        {killerNumber}
+        {' '}
+      </h2>
+      <UserInfo
+        killerNumber={killerNumber}
+        showAll={showAll}
+      />
+      <button onClick={dec}>-</button>
+      <button onClick={inc}>+</button>
+      <button onClick={show}>Show All Killers</button>
+      <button onClick={unshow}>Show personal card</button>
     </div>
   );
-}
+};
 
 export default App;
